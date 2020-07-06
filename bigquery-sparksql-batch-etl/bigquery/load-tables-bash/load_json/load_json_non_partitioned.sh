a1d92d5479cd8e07f7a1cd354f6e25a8a95a6c25
kayne10 <troy.kayne@colorado.edu>
2020-07-05 19:19:34 -0600

    practiced creating partitioned tables in big query bucket storage

--- bigquery-sparksql-batch-etl/bigquery/load-tables-bash/load_json/load_json_non_partitioned.sh
+++ bigquery-sparksql-batch-etl/bigquery/load-tables-bash/load_json/load_json_non_partitioned.sh
@@ -5,10 +5,10 @@ data_analysis.flight_delays_json_non_partitioned &&
 
 bq load --source_format=NEWLINE_DELIMITED_JSON  \
  data_analysis.flight_delays_json_non_partitioned \
- gs://your_bucket_name/2019-04-27.json
+ gs://etl-practice/2019-04-27.json
 
 
 
  bq load --source_format=NEWLINE_DELIMITED_JSON --autodetect \
  data_analysis.flight_delays_json_non_partitioned \
- gs://your_bucket_name/2019-04-27.json
+ gs://etl-practice/2019-04-27.json
